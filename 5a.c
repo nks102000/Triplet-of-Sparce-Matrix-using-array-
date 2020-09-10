66606b323229ac55bc10202c36821c6b0a9deb69
NITISH KUMAR SONTHALIA <53121643+nks102000@users.noreply.github.com>
2020-09-10 18:02:23 +0530

    Add files via upload

---
+++ 5a.c
@@ -0,0 +1,42 @@
+//Nitish Kumar Sonthalia
+//Triplet of Sparce Matrix using array 
+#include<stdio.h> 
+
+int main() 
+{ 
+	int sparseMatrix[4][5] = 
+	{ 
+		{0 , 2 , 0, 25 , 0 }, 
+		{0 , 0 , 14 , 0 , 0 }, 
+		{0 , 20 , 0 , 8 , 0 }, 
+		{0 , 0 , 89 , 0 , 0 } 
+	}; 
+
+	int size = 0; 
+	for (int i = 0; i < 4; i++) 
+		for (int j = 0; j < 5; j++) 
+			if (sparseMatrix[i][j] != 0) 
+				size++; 
+
+	int compactMatrix[3][size]; 
+
+	int k = 0; 
+	for (int i = 0; i < 4; i++) 
+		for (int j = 0; j < 5; j++) 
+			if (sparseMatrix[i][j] != 0) 
+			{ 
+				compactMatrix[0][k] = i; 
+				compactMatrix[1][k] = j; 
+				compactMatrix[2][k] = sparseMatrix[i][j]; 
+				k++; 
+			} 
+
+	for (int i=0; i<3; i++) 
+	{ 
+		for (int j=0; j<size; j++) 
+			printf("%d ", compactMatrix[i][j]); 
+
+		printf("\n"); 
+	} 
+	return 0; 
+} 
